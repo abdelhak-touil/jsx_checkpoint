3f7548c4bd6459e5ee1abaf8de247d1ec7eaa21a
abdelhak-touil <abdelhak.touil1994@gmail.com>
2021-03-13 19:25:01 +0100

    checkpoint jsx

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import imageInSrc from "./assets/img/imageInSrc.png";
+import imageInPublic from "./assets/img/imageInPublic.jpg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App-header">
+      <div style={{ border: "solid 1px black", maxWidth: "100vw" }}>
+        <h1 className="title red">TOUIL Abdelhak</h1>
+        <br />
+        <img src={imageInSrc} alt="logo" />
+        <br />
+        <div style={{ display: "flex", justifyContent: "center" }}>
+          <img src={imageInPublic} alt="logo2" />
+        </div>
+      </div>
+      <div>
+        <video
+          controls
+          autoPlay
+          currentTime={11.3}
+          src={
+            "https://s3.amazonaws.com/codecademy-content/courses/React/react_video-fast.mp4"
+          }
+        />
+      </div>
     </div>
   );
 }
